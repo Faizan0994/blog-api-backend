c128a22719c0e6e96b8b836798e686a5740b96d1
faizan <faizanamjad575@gmail.com>
2026-01-31 12:56:56 +0500

    set up express

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
+
+const app = express();
+
+app.listen(process.env.PORT || 3000, (error) => {
+  console.log("server listening...");
+  if (error) {
+    console.log(error);
+  }
+});
