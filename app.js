5a012066df6e3f623b649697327d50e6227caeea
faizan <faizanamjad575@gmail.com>
2026-02-02 21:25:27 +0500

    implement auth router

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const path = require("path");
+const jwt = require("jsonwebtoken");
+const authRouter = require("./routes/auth");
 require("dotenv").config();
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRouter);
 
 app.listen(process.env.PORT || 3000, (error) => {
   console.log("server listening...");
